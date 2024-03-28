40d62dc99f43625e7b216c790cde6a7658ed1cee
Zarkasi31 <zarkasi31@gmail.com>
2024-03-28 16:28:40 +0800

    day-13

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const tombol = document.querySelector('.bxs-plus-circle');
 const layar = document.querySelector('.layar');
+const balik = document.querySelector('.bx-plus-medical');
 
 tombol.addEventListener('click', function(){
     layar.classList.toggle('muncul');
+})
+
+balik.addEventListener('click', function(){
+    layar.classList.remove('muncul');
 })
