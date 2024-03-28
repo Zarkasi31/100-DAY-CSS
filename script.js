5b8cba81d22fb88ee440bccc62491ad10b14995c
Zarkasi31 <zarkasi31@gmail.com>
2024-03-28 18:44:15 +0800

    day-13

--- script.js
+++ script.js
@@ -4,21 +4,27 @@ const tombol3 = document.querySelector('.tombol-3');
 const tombol4 = document.querySelector('.tombol-4');
 const layar = document.querySelector('.layar');
 const balik = document.querySelector('.x');
+const foto = document.querySelector('.foto-profil');
 
 tombol.addEventListener('click', function(){
     layar.classList.toggle('muncul');
+    foto.src='foto1.jpg';
+
 })
 
 tombol2.addEventListener('click', function(){
     layar.classList.toggle('muncul');
+    foto.src='foto2.jpg';
 })
 
 tombol3.addEventListener('click', function(){
     layar.classList.toggle('muncul');
+    foto.src='foto3.jpg';
 })
 
 tombol4.addEventListener('click', function(){
     layar.classList.toggle('muncul');
+    foto.src='foto4.jpg';
 })
 
 balik.addEventListener('click', function(){
